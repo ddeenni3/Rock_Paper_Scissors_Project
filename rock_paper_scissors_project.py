a08877ce0bcdc2e0cb68f729f36a5477de40c100
ddeenni3 <d.dimitrovv3@gmail.com>
2023-09-24 23:55:37 +0300

    Adding the source code of the project.

--- rock_paper_scissors_project.py
+++ rock_paper_scissors_project.py
@@ -1,27 +1,97 @@
 import random
+from colorama import Fore
+import time
+
+print('''
+⠀⠀⠀⠀⠀⣠⡴⠖⠒⠲⠶⢤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⡴⠖⠒⢶⣄⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⢀⡾⠁⠀⣀⠔⠁⠀⠀⠈⠙⠷⣤⠦⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡼⠋⠀⠀⠀⢀⡿⠀⠀⠀⠀⠀⠀⠀
+⣠⠞⠛⠛⠛⠋⠉⠀⠀⠀⠀⠀⠀⠀⠀⠘⢧⠈⢿⡀⢠⡶⠒⠳⠶⣄⠀⠀⠀⠀⠀⣴⠟⠁⠀⠀⠀⣰⠏⠀⢀⣤⣤⣄⡀⠀⠀
+⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠟⠛⠛⠃⠸⡇⠈⣇⠸⡇⠀⠀⠀⠘⣇⠀⠀⣠⡾⠁⠀⠀⠀⢀⣾⣣⡴⠚⠉⠀⠀⠈⠹⡆⠀
+⣹⡷⠤⠤⠤⠄⠀⠀⠀⠀⢠⣤⡤⠶⠖⠛⠀⣿⠀⣿⠀⢻⡄⠀⠀⠀⢻⣠⡾⠋⠀⠀⠀⠀⣠⡾⠋⠁⠀⠀⠀⠀⢀⣠⡾⠃⠀
+⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⡤⠖⠋⢀⣿⣠⠏⠀⠀⣿⠀⠀⠀⠘⠉⠀⠀⠀⠀⠀⡰⠋⠀⠀⠀⠀⠀⣠⠶⠋⠁⠀⠀⠀
+⢿⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⡾⠋⠁⠀⠀⠠⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠐⠁⠀⠀⠀⢀⣴⡿⠥⠶⠖⠛⠛⢶⡄
+⠀⠉⢿⡋⠉⠉⠁⠀⠀⠀⠀⠀⢀⣠⠾⠋⠀⠀⠀⠀⢀⣰⡇⠀⠀⢀⡄⠀⠀⠀⠀⠀⠀⠀⠀⢀⡴⠋⠀⠀⠀⠀⠀⢀⣠⠼⠃
+⠀⠀⠈⠛⠶⠦⠤⠤⠤⠶⠶⠛⠋⠁⠀⠀⠀⠀⠀⠀⣿⠉⣇⠀⡴⠟⠁⣠⡾⠃⠀⠀⠀⠀⠀⠈⠀⠀⠀⣀⣤⠶⠛⠉⠀⠀⠀
+⠀⠀⠀⠀⢀⣠⣤⣀⣠⣤⠶⠶⠒⠶⠶⣤⣀⠀⠀⠀⢻⡄⠹⣦⠀⠶⠛⢁⣠⡴⠀⠀⠀⠀⠀⠀⣠⡶⠛⠉⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⢀⡴⠋⣠⠞⠋⠁⠀⠀⠀⠀⠙⣄⠀⠙⢷⡀⠀⠀⠻⣄⠈⢷⣄⠈⠉⠁⠀⠀⠀⢀⣠⡴⠟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⢀⡾⠁⣴⠋⠰⣤⣄⡀⠀⠀⠀⠀⠈⠳⢤⣼⣇⣀⣀⠀⠉⠳⢤⣭⡿⠒⠶⠶⠒⠚⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⢸⠃⢰⠇⠰⢦⣄⡈⠉⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠉⠉⠛⠛⠓⠲⢦⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠸⣧⣿⠀⠻⣤⡈⠛⠳⠆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠈⠹⣆⠀⠈⠛⠂⠀⠀⠀⠀⠀⠀⠈⠐⠒⠒⠶⣶⣶⠶⠤⠤⣤⣠⡼⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠹⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠳⢦⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠈⠻⣦⣀⠀⠀⠀⠀⠐⠲⠤⣤⣀⡀⠀⠀⠀⠀⠀⠉⢳⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⠶⠤⠤⠤⠶⠞⠋⠉⠙⠳⢦⣄⡀⠀⠀⠀⡷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀    ⠈⠙⠳⠦⠾⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+''')
 
 valid_options = ['r', 'p', 's']
 
-player_move = input('Choose [r]ock, [p]aper or [s]cissors: ').lower()
+games_counter = 0
+player_score = 0
+computer_score = 0
+draw_score = 0
+best_streak = 0
 
-if player_move in valid_options:
-    computer_rng = random.randrange(len(valid_options))
-    computer_move = valid_options[computer_rng]
-    computer_print = ''
-    if computer_move == 'r':
-        computer_print = 'rock'
-    elif computer_move == 'p':
-        computer_print = 'paper'
-    elif computer_move == 's':
-        computer_print = 'scissors'
-    print(f'The computer has chosen {computer_print}')
-    if player_move == computer_move:
-        print('Draw!')
-    elif (player_move == 's' and computer_move == 'p') or\
-            (player_move == 'r' and computer_move == 's') or\
-            (player_move == 'p' and computer_move == 'r'):
-        print('You win!')
+
+while True:
+    player_choice = input(Fore.LIGHTWHITE_EX + 'Press "Enter" to play or "q" to quit: ').lower()
+    if player_choice == 'q':
+        print('Thanks for playing!')
+        break
+    elif player_choice == '':
+        player_move = input('Choose [r]ock, [p]aper or [s]cissors: ').lower()
+        if player_move in valid_options:
+            print('Rock...')
+            time.sleep(0.3)
+            print('Paper...')
+            time.sleep(0.3)
+            print('Scissors...')
+            time.sleep(0.3)
+            print('GO!')
+            time.sleep(0.2)
+            computer_rng = random.randrange(len(valid_options))
+            computer_move = valid_options[computer_rng]
+            computer_print = ''
+            player_print = ''
+            if computer_move == 'r':
+                computer_print = 'rock'
+            elif computer_move == 'p':
+                computer_print = 'paper'
+            elif computer_move == 's':
+                computer_print = 'scissors'
+            if player_move == 'r':
+                player_print = 'rock'
+            elif player_move == 'p':
+                player_print = 'paper'
+            elif player_move == 's':
+                player_print = 'scissors'
+            print(f'Your choice is: {Fore.GREEN + player_print}')
+            print(Fore.LIGHTWHITE_EX + f'The computer has chosen {Fore.BLUE + computer_print}')
+            if player_move == computer_move:
+                print(Fore.YELLOW + 'Draw!')
+                draw_score += 1
+                best_streak = 0
+            elif (player_move == 's' and computer_move == 'p') or\
+                    (player_move == 'r' and computer_move == 's') or\
+                    (player_move == 'p' and computer_move == 'r'):
+                print(Fore.GREEN + 'You win!')
+                player_score += 1
+                best_streak += 1
+            else:
+                print(Fore.RED + 'You lose!')
+                computer_score += 1
+                best_streak = 0
+            games_counter += 1
+            print(Fore.CYAN + f'Total games: {games_counter}')
+            print(Fore.CYAN + f'Score: Player: {player_score} : {computer_score} Computer')
+            print(f'Draws: {draw_score}')
+            print(f'Best streak: {best_streak}')
+        else:
+            print('Invalid input. Try again.')
+            continue
     else:
-        print('You lose!')
-else:
-    raise SystemExit('Invalid input. Try again.')
+        print(Fore.RED + 'Invalid input. Try again')
+        continue
+
+
+
